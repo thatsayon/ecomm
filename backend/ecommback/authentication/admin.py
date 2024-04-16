737d1d39b693e07dc0df888eadf5b828a87ffff2
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2024-04-16 23:05:24 +0600

    user account model created

--- backend/ecommback/authentication/admin.py
+++ backend/ecommback/authentication/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from authentication.models import UserAccount 
 
-# Register your models here.
+admin.site.register(UserAccount)
--- backend/ecommback/authentication/models.py
+++ backend/ecommback/authentication/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _ 
+from django.utils import timezone
 
-# Create your models here.
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, email, password, **extra_kwargs):
+        if not email:
+            raise ValueError("email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_kwargs):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True"))
+        return self.create_user(email, password, **extra_kwargs)
+
+
+class UserAccount(AbstractBaseUser):
+    email = models.EmailField(_("email address"), unique=True)
+    full_name = models.CharField(_("full name"), max_length=255)
+    date_of_birth = models.DateField(_("date of birth"))
+    gender = models.CharField(_("gender"), max_length=10, choices=[(
+        "male", "Male"), ("female", "Female"), ("other", "Other")])
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email", "full_name", "date_of_birth", "gender"]
+
+    objects = CustomAccountManager()
+
+    def has_perm(self, perm, obj=None):
+        return self.is_staff 
+
+    def has_module_perms(self, app_level):
+        return self.is_staff 
+
+    def __str__(self):
+        return self.email 
+
